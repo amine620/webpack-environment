4c1a61403805381f4d5d8140e29acd60c0e3c116
amine620 <mohafidi1998@gmail.com>
2022-06-18 02:18:24 -1200

    composition

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import {compose,pipe} from "lodash/fp"
+
+
+let input ="javascript"
+
+const trim=str=>str.trim()
+const wrapInDiv=str=>`<div>${str}</div>`
+const toLowerCase=str=>str.toLowerCase()
+
+const transform=compose(toLowerCase,wrapInDiv,trim)
+transform(input)
+// const result=toLowerCase(wrapInDiv(trim(input)))
