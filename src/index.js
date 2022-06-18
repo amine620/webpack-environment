78a2784f50ef05790253e5034e8ce8314daf1391
amine620 <mohafidi1998@gmail.com>
2022-06-18 06:14:31 -1200

    create store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-const numbers=[1,2,3];
+import { store } from "./store";
 
-// adding
-
-const index=numbers.indexOf(2)
-const added=[...numbers.slice(0,index),4,...numbers.slice(index)]
-
-console.log(added);
+console.log(store);
---
+++ src/reducer.js
@@ -0,0 +1,19 @@
+
+let lastId=0
+export function reducer(state=[],action){
+    switch (action.type) {
+        case "bugAdded":
+            return[
+                ...state,
+                {
+                    id:++lastId,
+                    description:action.payload.description,
+                    resolved:false
+                }
+            ]
+            
+            break;
+        default:state
+            break;
+    }
+}
